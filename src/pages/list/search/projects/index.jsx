8786c33a6260b830daa92736322a4f95a3becef9
ivy0802 <ivy0802@gmail.com>
2019-10-22 09:51:08 +0200

    remove the searching bar

--- src/pages/list/search/projects/index.jsx
+++ src/pages/list/search/projects/index.jsx
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import { connect } from 'dva';
 import moment from 'moment';
 import AvatarList from './components/AvatarList';
-import StandardFormRow from './components/StandardFormRow';
-import TagSelect from './components/TagSelect';
 import styles from './style.less';
 
 const { Option } = Select;
@@ -95,71 +93,6 @@ class Projects extends Component {
     };
     return (
       <div className={styles.coverCardList}>
-        <Card bordered={false}>
-          <Form layout="inline">
-            <StandardFormRow
-              title="所属类目"
-              block
-              style={{
-                paddingBottom: 11,
-              }}
-            >
-              <FormItem>
-                {getFieldDecorator('category')(
-                  <TagSelect expandable>
-                    <TagSelect.Option value="cat1">类目一</TagSelect.Option>
-                    <TagSelect.Option value="cat2">类目二</TagSelect.Option>
-                    <TagSelect.Option value="cat3">类目三</TagSelect.Option>
-                    <TagSelect.Option value="cat4">类目四</TagSelect.Option>
-                    <TagSelect.Option value="cat5">类目五</TagSelect.Option>
-                    <TagSelect.Option value="cat6">类目六</TagSelect.Option>
-                    <TagSelect.Option value="cat7">类目七</TagSelect.Option>
-                    <TagSelect.Option value="cat8">类目八</TagSelect.Option>
-                    <TagSelect.Option value="cat9">类目九</TagSelect.Option>
-                    <TagSelect.Option value="cat10">类目十</TagSelect.Option>
-                    <TagSelect.Option value="cat11">类目十一</TagSelect.Option>
-                    <TagSelect.Option value="cat12">类目十二</TagSelect.Option>
-                  </TagSelect>,
-                )}
-              </FormItem>
-            </StandardFormRow>
-            <StandardFormRow title="其它选项" grid last>
-              <Row gutter={16}>
-                <Col lg={8} md={10} sm={10} xs={24}>
-                  <FormItem {...formItemLayout} label="作者">
-                    {getFieldDecorator('author', {})(
-                      <Select
-                        placeholder="不限"
-                        style={{
-                          maxWidth: 200,
-                          width: '100%',
-                        }}
-                      >
-                        <Option value="lisa">王昭君</Option>
-                      </Select>,
-                    )}
-                  </FormItem>
-                </Col>
-                <Col lg={8} md={10} sm={10} xs={24}>
-                  <FormItem {...formItemLayout} label="好评度">
-                    {getFieldDecorator('rate', {})(
-                      <Select
-                        placeholder="不限"
-                        style={{
-                          maxWidth: 200,
-                          width: '100%',
-                        }}
-                      >
-                        <Option value="good">优秀</Option>
-                        <Option value="normal">普通</Option>
-                      </Select>,
-                    )}
-                  </FormItem>
-                </Col>
-              </Row>
-            </StandardFormRow>
-          </Form>
-        </Card>
         <div className={styles.cardList}>{cardList}</div>
       </div>
     );
